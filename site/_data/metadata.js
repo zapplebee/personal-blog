4b0d9844b634838c7de2976e5e320f3e9041104e
Zachary Skalko <zac_skalko@cargill.com>
2021-12-06 10:04:46 -0600

    Use env for metadata

--- site/_data/metadata.js
+++ site/_data/metadata.js
@@ -1,6 +1,5 @@
-module.exports = {
-  FQDN: "zapplebee.github.io",
-  protocol: "https://",
-  // FQDN: "localhost:8080",
-  // protocol: "http://",
-};
+const config =
+  process.env.NODE_ENV === "production"
+    ? { FQDN: "zapplebee.github.io", protocol: "https://" }
+    : { FQDN: "localhost:8080", protocol: "http://" };
+module.exports = config;
